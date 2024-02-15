c49e04dbe3af5e013e8ca4d8a08283f6fcd2a0d5
James-Lycett <jrlycett@mail.com>
2024-02-14 20:06:04 -0800

    created some sample data to use in ./frontend/src/utils/api.js, wrote out some basic html for AdminHome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import LandingPage from "./landingPage/LandingPage"
+import LandingPage from "./LandingPage/LandingPage.js"
+import AdminHome from "./AdminHome/AdminHome.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />}/>
+        <Route path="/admin/home" element={<AdminHome />}/>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/adminHome/AdminHome.js
+++ frontend/src/adminHome/AdminHome.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { employees } from "../utils/api";
+import EmployeesList from "./EmployeesList";
 
 function AdminHome() {
 
+    return (
+        <>
+            <div>
+                <h2>Employee Sleep Quality Goals</h2>
+            </div>
+            <div>
+                <h2>Monthly Client Tasks Met</h2>
+            </div>
+            <div>
+                <h2>Monthly Revenue Goals</h2>
+            </div>
+            <div>
+                <h2>Your Employees</h2>
+                <a href="">Register A New Employee</a>
+                <div>
+                    <EmployeesList employees={employees}/>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default AdminHome
