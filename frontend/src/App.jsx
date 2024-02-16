0180a732a8c9f7b7dcf02dd947cc132212974200
Odonchimeg Myagmar <112740790+odnodeveloper@users.noreply.github.com>
2024-02-16 15:01:13 -0600

    Updated Landing & Login page, created Create Account page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import LandingPage from "./LandingPage/LandingPage.jsx"
-import AdminHome from "./AdminHome/AdminHome.jsx";
+import LandingPage from "./LandingPage/LandingPage"
+import AdminHome from "./AdminHome/AdminHome";
+import LoginPage from "./LoginPage/LoginPage";
+import CreateAccount from "./CreateAccount/CreateAccount";
+import UserHome from "./UserHome/UserHome";
 
 export default function App() {
   return (
@@ -8,6 +12,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/admin/home" element={<AdminHome />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<CreateAccount />} />
+        <Route path="/user/home" element={<UserHome />} />
       </Routes>
     </BrowserRouter>
   )
