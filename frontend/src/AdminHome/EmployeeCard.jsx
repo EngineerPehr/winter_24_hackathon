0b1afac6eec4a1e27293a92d83213eb579f3c1b6
James-Lycett <jrlycett@mail.com>
2024-02-15 21:54:07 -0800

    added X and 'hide' buttons to EmployeeCard, not sure yet what to do with X, hide toggles that card's visibility (collapses it)

--- frontend/src/AdminHome/EmployeeCard.jsx
+++ frontend/src/AdminHome/EmployeeCard.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import sleepingPersonImage from "./sleeping_person.jpg";
 
 export default function EmployeeCard({ employee }) {
-    return (
-        <>
-            <img src={sleepingPersonImage} style={{ width: "100px"}} alt="sleeping person"></img>
-            <h3>{employee.name}</h3>
-            <h4>Age: {employee.age}</h4>
-            <h4>Gender: {employee.gender}</h4>
-            <h4>Sleep Hours: {employee.sleep_duration}</h4>
-        </>
-    )
+    const [hidden, setHidden] = useState(false)
+
+    const toggleVisibility = () => {
+        hidden ? setHidden(false) : setHidden(true)
+    }
+
+    if (hidden) {
+        return <button onClick={toggleVisibility}>Show</button>
+    } else {
+        return (
+            <>
+            <div>
+                <img src={sleepingPersonImage} style={{ width: "100px"}} alt="sleeping person"></img>
+            </div>
+            <div>
+                <h3>{employee.name}</h3>
+                <h4>Age: {employee.age}</h4>
+                <h4>Gender: {employee.gender}</h4>
+                <h4>Sleep Hours: {employee.sleep_duration}</h4>
+            </div>
+            <div>
+                <button>X</button>
+                <button onClick={toggleVisibility} >Hide</button>
+            </div>
+            </>
+        )
+    }
 }
