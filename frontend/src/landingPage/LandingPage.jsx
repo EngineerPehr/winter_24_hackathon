1b485b58bdee4098bfeb1f3290208e2ed7c4e000
James-Lycett <jrlycett@mail.com>
2024-02-18 20:57:38 -0800

    fixed double copy of styles on LandingPage

--- frontend/src/landingPage/LandingPage.jsx
+++ frontend/src/landingPage/LandingPage.jsx
@@ -19,15 +19,14 @@ export default function LandingPage() {
                     <div className="flex flex-col items-center justify-center">
                         <p className="text-2xl mt-8 mb-6">Already a member?</p>
                         <button                             
-                             className="mb-3 block w-1/2 rounded border-2 border-black px-6 pb-[6px] pt-2 text-xl font-bold uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10
-                             border-black focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-xl px-5 py-1 me-2 mb-2"
+                             className="mb-3 block w-1/2 rounded border-2 border-black px-6 pb-[6px] pt-2 text-xl font-bold uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                             
                              data-te-ripple-init>
                             <Link to="/login">SIGN IN</Link>
                         </button>
                         <p className="text-2xl my-5 mt-10">Don't have an account?</p>
                         <button 
-                            className="mb-10 block w-1/2 rounded border-2 border-black px-6 pb-[6px] pt-2 text-xl font-bold uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10
-                            border-black focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-xl px-5 py-1 me-2 mb-2"
+                            className="mb-10 block w-1/2 rounded border-2 border-black px-6 pb-[6px] pt-2 text-xl font-bold uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                             data-te-ripple-init>
                             <Link to="/register">REGISTER</Link>
                         </button> 
