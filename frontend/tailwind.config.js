d4e8d388299bc2ca2c6b7ff601e9d27223c58a4b
James-Lycett <jrlycett@mail.com>
2024-02-19 18:39:43 -0800

    set up buttons classes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,14 +12,16 @@ module.exports = {
       colors: {
         primary: {
           1: '#000000',
-          2: '#F1F0F0',
+          2: '#FFFFFF',
           3: '#0B1997',
-          4: '#6197BE',
+          4: '#00528E',
+          5: '#FFFFFF',
         },
         accent: {
           1: '#51565A',
-          2: '#022B69',
-          background: 'D9D9D9'
+          2: '#022860',
+          3: '#F0F0F0',
+          background: '#E2EFF2'
         },
 
       },
