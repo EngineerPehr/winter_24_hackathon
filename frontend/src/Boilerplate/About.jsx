266bbd68db6fc61d8425939c8dc8ac5d70361062
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-02-21 07:32:44 -0500

    manually added boilerplate files

---
+++ frontend/src/Boilerplate/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <section className="max-w-screen-md mx-auto mt-8 p-6 bg-white shadow-md">
+                <h5 className="text-3xl font-bold text-center">
+                    Welcome to Workplace Wellness
+                </h5>
+                <br />
+                <p>
+                    Embark on a transformative journey with Workplace Wellness,
+                    where your pursuit of improved sleep quality and enhanced
+                    mental wellness takes center stage. At Workplace Wellness,
+                    we understand the profound impact that sleep has on your
+                    overall well-being. Our dedicated team is committed to
+                    providing you with the tools and resources necessary to
+                    unlock the secrets to a restful night's sleep and cultivate
+                    mental resilience.
+                </p>
+                <br />
+                <h5 className="text-xl text-center">Our Mission</h5>
+                <br />
+                <p>
+                    At the core of Workplace Wellness is a mission to empower
+                    individuals on their path to better mental health. We
+                    recognize the challenges posed by stress, anxiety, and
+                    insomnia, and we are here to guide you through
+                    evidence-based practices and expertly curated articles. Our
+                    mission extends beyond a simple sleep routine; we aim to
+                    foster a community where compassion and information
+                    converge, creating a space where you can truly thrive.
+                </p>
+                <br />
+                <h5 className="text-xl text-center">Your Path to Better</h5>
+                <br />
+                <p>
+                    Sleep Workplace Wellness serves as your ally in the pursuit
+                    of better sleep and enhanced mental resilience. We invite
+                    you to take the first step on your path to improved
+                    well-being by exploring the resources and community we
+                    offer. Whether you're a seasoned advocate for mental health
+                    or just beginning your journey, we are here to guide you
+                    towards a healthier, more restful life. At Workplace
+                    Wellness, we believe that prioritizing your mental health is
+                    a transformative journey—one that starts with a good night's
+                    sleep. Join us, and let's navigate this path together,
+                    embracing the profound impact that quality sleep can have on
+                    your overall wellness. Welcome to Workplace Wellness, where
+                    your journey to better sleep and mental resilience begins.
+                </p>
+            </section>
+        </>
+    )
+}
+
+export default About
